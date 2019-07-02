35a9d8e549897c6f860d1931116fba9d5feae991
Jirka Nagy <jirka.nagy@gmail.com>
2019-07-02 23:26:59 +0200

    Add image component

---
+++ Image/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img src="{{ src }}" />
+  </div>
+</template>
+<script>
+export default {
+  name: "Image",
+  props: {
+    src: {
+      type: "String",
+      default: "https://picsum.photos/400"
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
