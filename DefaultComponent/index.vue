f305be1eb7f9b22c9ab79ebc3a8484d5a222bb5f
Kryštof Rainisch <krystof.rainisch@sprites.co>
2019-07-02 15:40:09 +0200

    first component

---
+++ DefaultComponent/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-show="isVisible">Ahoj!</div>
+    <button @click="toggle">Click me</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Default Component",
+  data: {
+    isVisible: false
+  },
+  methods: {
+    toggle() {
+      this.isVisible = !this.isVisible
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
