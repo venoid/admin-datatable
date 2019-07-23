e0a0051b8bd0dd61c98d2f1ed1c28c0777c4005c
Dmitrij Tkačenko <dimot9@gmail.com>
2019-07-23 23:02:18 +0200

    :pencil: Update documents - Update readme - update package.json - edit publish.bash - remove node_modules

--- publish.bash
+++ publish.bash
@@ -4,25 +4,7 @@
 npm run lint
 [[ $? -eq 1 ]] && exit || echo "Lint OK"
 
-# Show current version
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
-
-# Get package version
-#echo Enter new package version - current is $PACKAGE_VERSION :
-#read NEW_VERSION
-
-# Set new version
-#npm version $NEW_VERSION
-
-# Login to npm
-#npm login
-
 # Build project and publish library with new version
-npm run build-bundle && yarn publish
+npm run build-bundle && yarn publish --access public
 
-#git push
+git push
---
+++ src/components/Image/README.md
@@ -0,0 +1,8 @@
+# Image component
+
+Dynamic Vue component using html img tag...
+
+## Props
+
+### src
+- url of an image
