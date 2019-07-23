25811b4f1cc6840fa890410c7e330f2a8b3291a0
Dmitrij Tkačenko <dimot9@gmail.com>
2019-07-23 16:27:47 +0200

    :sparkles: Add publish script

--- publish.bash
+++ publish.bash
@@ -4,5 +4,22 @@
 npm run lint
 [[ $? -eq 1 ]] && exit || echo "Lint OK"
 
-# Build project and publish library
-npm run build-bundle && yarn publish
+# Show current version
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+# Get package version
+echo Enter new package version - current is $PACKAGE_VERSION :
+read NEW_VERSION
+
+# Set new version
+npm version $NEW_VERSION
+
+# Build project and publish library with new version
+npm run build-bundle && npm publish --access public
+
+git push
