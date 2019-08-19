25f50850df6afe1880c7c8cc5356399b089a717e
Dmitrij Tkačenko <dimot9@gmail.com>
2019-08-19 15:30:34 +0200

    :recycle: Get rid of old structure

--- src/components/Image/README.md
+++
@@ -1,8 +0,0 @@
-# Image component
-
-Dynamic Vue component using html img tag...
-
-## Props
-
-### src
-- url of an image
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import DefaultComponent from "./DefaultComponent/DefaultComponent"
-import Image from "./Image/Image"
-
-export const fresh_image = Image;
-export const fresh_default = DefaultComponent;
