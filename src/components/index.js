8615655d434eeb79440f256405732e59202e8096
Dmitrij Tkačenko <dimot9@gmail.com>
2019-08-19 15:02:02 +0200

    :tada: Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import DefaultComponent from "./DefaultComponent/DefaultComponent"
+import Image from "./Image/Image"
+
+export const fresh_image = Image;
+export const fresh_default = DefaultComponent;
